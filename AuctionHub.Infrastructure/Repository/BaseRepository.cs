ba44855a2d16ee6476b73990d1a5636c2119286f
Rafael Todescato <rafael_todescato@hotmail.com>
2026-04-15 00:14:27 -0400

    feat: added base structure project

---
+++ AuctionHub.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using AuctionHub.Domain.Interfaces.Repositories;
+using AuctionHub.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AuctionHub.Infrastructure.Repository
+{
+    internal class BaseRepository<T>(AuctionHubContext context) : IBaseRepository<T> where T : class
+    {
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default)
+        {
+            return context.Set<T>().AnyAsync(predicate, ct);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>().Where(predicate);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>().AsQueryable();
+        }
+    }
+}
