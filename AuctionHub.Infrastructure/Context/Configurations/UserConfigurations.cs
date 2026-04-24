74dc9ae1e4bf3a1794c43ba622854851779b59ba
Rafael Todescato <rafael_todescato@hotmail.com>
2026-04-24 15:38:33 -0400

    fix: solved password hashing problem

--- AuctionHub.Infrastructure/Context/Configurations/UserConfigurations.cs
+++ AuctionHub.Infrastructure/Context/Configurations/UserConfigurations.cs
@@ -11,7 +11,7 @@ namespace AuctionHub.Infrastructure.Context.Configurations
         {
             builder.HasIndex(x => x.Email).IsUnique();
 
-            builder.HasData(new User(1, "Admin", "admin@gmail.com", "nC1lP9vY8r0mX9V2k9v6W7sJ9H0zF6lYk8d8l6V6m0Q=", ERole.ADMIN, EUserStatus.ACTIVE, new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)));
+            builder.HasData(new User(1, "Admin", "admin@gmail.com", "VTy80B1UufqFBbxLspeoQqM6wTwuNvspE6uYszXi/4s=", ERole.ADMIN, EUserStatus.ACTIVE, new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)));
         }
     }
 }
